eac66f2b7b8d4593786abda8ceba910eb6609e8b
AlbusTheDev <qusaih42@gmail.com>
2024-08-18 09:51:18 +0300

    2.4

--- app/main.js
+++ app/main.js
@@ -4,7 +4,10 @@ function matchPattern(inputLine, pattern) {
     return inputLine.includes(pattern);
   } else if (pattern.startsWith("\\d")) {
 
-    return inputLine
+    for (let i = 0; i < inputLine.length; i++) {
+      const code = inputLine[i].charCodeAt(0);
+      if (charCode >= 49 && charCode <= 57) return true;
+    }
 
     return false;
 
@@ -32,14 +35,12 @@ function main() {
   }
 
   // You can use print statements as follows for debugging, they'll be visible when running tests.
-  console.log("Logs from your program will appear here");
+  //console.log("Logs from your program will appear here");
 
   // Uncomment this block to pass the first stage
   if (matchPattern(inputLine, pattern)) {
-    console.log(0);
     process.exit(0);
   } else {
-    console.log(1);
     process.exit(1);
   }
 }
